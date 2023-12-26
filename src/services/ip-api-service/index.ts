d3a5018419dbba09eb291e31c47476102a08f9f5
Mariano César D'Angelo <mcdangelo@teco.com.ar>
2023-12-26 17:17:22 -0300

    add lang

--- src/services/ip-api-service/index.ts
+++ src/services/ip-api-service/index.ts
@@ -3,7 +3,7 @@ import { fetchWithTimeout } from "../../utils";
 const baseUrl = process.env.IP_API_URL;
 
 export async function fetchIpApiData(ip: string) {
-  const url = `${baseUrl}/${ip}`;
+  const url = `${baseUrl}/${ip}?lang=es`;
   const data = await fetchWithTimeout(url);
   return data;
 }
